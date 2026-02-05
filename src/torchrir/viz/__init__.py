54e444e613ded57012bd942827f34c2af74a9b96
taishi-n <taishi.m.and.p@gmail.com>
2026-02-05 11:22:22 +0100

    Consolidate visualization helpers

--- src/torchrir/viz/__init__.py
+++ src/torchrir/viz/__init__.py
@@ -2,7 +2,7 @@
 
 from .animation import animate_scene_gif
 from .plotting import plot_scene_dynamic, plot_scene_static
-from .plotting_utils import plot_scene_and_save
+from .plotting import plot_scene_and_save
 
 __all__ = [
     "animate_scene_gif",
